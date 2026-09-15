dd4e997d71022c296b5e022d4659f652456941ff
Benn326 <bjzyygg@gmail.com>
2026-09-15 20:55:51 +0700

    Sửa lại bài 2

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
-
+app.json.ensure_ascii = False
 @app.route("/status", methods=["GET"])
 def check_status():
     return jsonify({"Shop:": "Đang mở", "Time": "8:00 - 22:00"}), 200
@@ -9,8 +9,8 @@ def check_status():
 @app.route("/order", methods=["POST"])
 def order():
     data = request.get_json(silent=True) or {}
-    food = data.get("Item", "Mỳ cay 21724 cấp độ")
-    return jsonify({"Message": f"Đã nhận đơn :{food}. Vui lòng chờ!"}), 200
+    food = data.get("item", "Mỳ cay 21724 cấp độ")
+    return jsonify({"message": f"Đã nhận đơn: {food}. Vui lòng chờ!"}), 200
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000,debug=True )
