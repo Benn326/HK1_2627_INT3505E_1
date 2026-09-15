9845142cced31b38dcb316fe3fa0d6dba0d5ee42
Benn326 <bjzyygg@gmail.com>
2026-09-15 22:26:34 +0700

    Bài thực hành 4

--- app.py
+++ app.py
@@ -23,5 +23,21 @@ def create_student():
     STUDENTS.append(student)
     return jsonify(student), 201
 
+@app.route("/students/<student_id>",methods=["GET"])
+def get_student(student_id):
+    for student in STUDENTS:
+        if student["id"] == student_id:
+            return jsonify(student), 200
+    return jsonify({"error": "ID không tồn tại"}), 404
+
+@app.route("/students", methods=["GET"])
+def list_students():
+    limit = int(request.args.get("limit", 20))
+    q = request.args.get("q", "").strip().lower()
+    results = STUDENTS
+    if q:
+        results = [s for s in STUDENTS if q in s["name"].lower()]
+
+    return jsonify({"items": results[:limit]}), 200
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
